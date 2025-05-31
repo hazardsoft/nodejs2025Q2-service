167deda8217ad56bc55f973a1bb716e185fdf130
Henadzi Shutko <hazardsoft@gmail.com>
2025-05-31 18:20:05 +0300

    feat: add validation decorators to track entity

--- src/tracks/dto/create-track.dto.ts
+++ src/tracks/dto/create-track.dto.ts
@@ -1,10 +1,15 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateTrackDto {
   @IsNotEmpty()
   name: string;
+
+  @IsOptional()
   artistId: string | null; // refers to Artist
+
+  @IsOptional()
   albumId: string | null; // refers to Album
+
   @IsNumber()
   duration: number; // integer number
 }
