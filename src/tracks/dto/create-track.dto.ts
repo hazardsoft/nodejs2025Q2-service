1201589267eb2df82b02a7afedb60025c88fe7d7
Henadzi Shutko <hazardsoft@gmail.com>
2025-06-01 06:21:39 +0300

    feat: add more validation rules to create track dto

--- src/tracks/dto/create-track.dto.ts
+++ src/tracks/dto/create-track.dto.ts
@@ -1,13 +1,15 @@
-import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateTrackDto {
   @IsNotEmpty()
   name: string;
 
   @IsOptional()
+  @IsString()
   artistId: string | null; // refers to Artist
 
   @IsOptional()
+  @IsString()
   albumId: string | null; // refers to Album
 
   @IsNumber()
