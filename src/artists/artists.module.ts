b979ae25c9e6d63670112e8a61a3b7861648f6c7
Henadzi Shutko <hazardsoft@gmail.com>
2025-06-10 03:12:45 +0300

    feat: connect artist endpoint to db via prisma

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
+import { ArtistsRepository } from './artists.repository';
+import { PrismaModule } from 'src/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ArtistsController],
-  providers: [ArtistsService],
+  providers: [ArtistsService, ArtistsRepository],
   exports: [ArtistsService],
 })
 export class ArtistsModule {}
